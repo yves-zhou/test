8ee31678c7cd5fabb3996610f292af2fbdd25bb9
周垚 <zhouyao@solex.com>
2017-07-05 16:45:33 +0800

    modify

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-        navigator.serviceWorker.register('./sw.js', {scope: '/'})
+        navigator.serviceWorker.register('./sw.js', {scope: './'})
             .then(function (registration) {
                 // 注册成功
                 console.log('ServiceWorker registration successful with scope: ', registration.scope);
